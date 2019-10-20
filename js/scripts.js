5491a4f55a761f8af2eac086018bfb2c95403aae
Christopher Magnemi <chrismagnemi@gmail.com>
2019-10-20 16:34:57 -0400

    set up git api for home page, cleaned up scripts.js so NYT api would also work

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
+// add Event listener everywhere
 document.addEventListener('click', clickHandlers);
+
+//trigger a click on load
+window.addEventListener('load', clickHandlers);
 
 var nyt =
   'https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=uQG4jhIEHKHKm0qMKGcTHqUgAolr1GM0';
+var github = 'https://api.github.com/users/ChrisMagnemi';
 
 function clickHandlers() {
-  if (event.target.matches('button')) {
-    getData();
+  if (document.body.classList == 'ajax') {
+    getData(nyt);
+    console.log('nyt');
+  } else if (document.body.classList == 'homegit') {
+    getData(github);
+    console.log('git');
+  } else {
+    return;
   }
 }
 
-var addContent = function(data) {
+var addContent_nyt = function(data) {
   var looped = '';
   for (let i = 0; i < data.results.length; i++) {
     looped += `
@@ -22,11 +33,29 @@ var addContent = function(data) {
       </div>
       `;
   }
-  document.querySelector('.content div').innerHTML = looped;
+  document.querySelector('.ajax .content div').innerHTML = looped;
 };
 
-var getData = function() {
-  fetch(nyt)
-    .then(response => response.json())
-    .then(json => addContent(json));
+var addContent_git = function(data) {
+  var gitdata = `
+  <h3><a href="${data.url}">${data.name}</a></h3>
+  <p>${data.bio}</p>
+  <img src="${data.avatar_url}" />
+  `;
+  document.querySelector('.homegit .content div').innerHTML = gitdata;
+};
+
+var getData = function(apiLink) {
+  switch (apiLink) {
+    case nyt:
+      fetch(nyt)
+        .then(response => response.json())
+        .then(json => addContent_nyt(json));
+      break;
+    case github:
+      fetch(github)
+        .then(response => response.json())
+        .then(json => addContent_git(json));
+      break;
+  }
 };
